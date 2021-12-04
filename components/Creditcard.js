077fa33477e1815e71e396dc5bf6a7a4cc8ce8a9
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2021-12-04 11:48:39 +0100

    Various stuff...

--- components/Creditcard.js
+++ components/Creditcard.js
@@ -25,20 +25,20 @@ export default function CreditCard(props) {
   }
 
   return (
-    <div id="PaymentForm" className="container">
+    <div id="PaymentForm" className="grid-2">
       <div className="column">
         <Cards cvc={cvc} expiry={expiry} focused={focus} name={name} number={cardNumber} />
       </div>
       <div className="column">
         <form ref={form} onSubmit={onSubmit}>
           <fieldset>
-            <label for="fullname">Full name</label>
+            <label htmlFor="fullname">Full name</label>
             <input id="fullname" type="text" placeholder="Aaron Aaronson" required onChange={handleNameChanged} name="name" value={name} onFocus={handleInputFocus} />
             <span className="hint">Please write your full name</span>
           </fieldset>
 
           <fieldset>
-            <label for="cardnumber">Card number</label>
+            <label htmlFor="cardnumber">Card number</label>
             <input
               id="cardnumber"
               type="tel"
@@ -49,18 +49,18 @@ export default function CreditCard(props) {
               value={cardNumber}
               onFocus={handleInputFocus}
               pattern="[\d| ]{16,19}"
-              maxlength="19"
+              maxLength="19"
               size="19"
             />
             <span className="hint">Please check you cardnumber</span>
           </fieldset>
 
           <fieldset>
-            <label for="expiry">Expiry</label>
-            <input id="expiry" type="tel" placeholder="01/23" required onChange={(e) => setExpiry(e.target.value)} name="expiry" value={expiry} onFocus={handleInputFocus} pattern="[\d|]{2}/?[\d|]{2}" maxlength="5" size="5" />
+            <label htmlFor="expiry">Expiry</label>
+            <input id="expiry" type="tel" placeholder="01/23" required onChange={(e) => setExpiry(e.target.value)} name="expiry" value={expiry} onFocus={handleInputFocus} pattern="[\d|]{2}/?[\d|]{2}" maxLength="5" size="5" />
 
-            <label for="cvc">CVC</label>
-            <input id="cvc" type="tel" placeholder="123" required onChange={(e) => setCvc(e.target.value)} name="cvc" value={cvc} onFocus={handleInputFocus} pattern="[\d|]{3}" maxlength="3" size="3" />
+            <label htmlFor="cvc">CVC</label>
+            <input id="cvc" type="tel" placeholder="123" required onChange={(e) => setCvc(e.target.value)} name="cvc" value={cvc} onFocus={handleInputFocus} pattern="[\d|]{3}" maxLength="3" size="3" />
             <span className="hint">Please enter the expiry date of your card (4 digits)</span>
             <span className="hint">Please enter the CVC number of your card (3 digits)</span>
             <span className="hint-double">Please enter the correct expiry date and CVC number of your card</span>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Countdown from "../components/Countdown";
 
 export default function Dashboard() {
   return (
@@ -7,8 +8,67 @@ export default function Dashboard() {
         <title>Foobar</title>
         <meta name="description" content="A school project" />
       </Head>
+
       <main>
-        <h1>Here is a dashboard with lots of nice data</h1>
+        <h1 className="center">Foobar</h1>
+        <Countdown />
+        <section className="grid-2-mobile">
+          <div className="column sec-bg">
+            <h2 className="center">Now serving</h2>
+            <div className="flex-wrap">
+              <p className="terti-bg sec-txt">101</p>
+              <p className="terti-bg sec-txt">102</p>
+              <p className="terti-bg sec-txt">103</p>
+            </div>
+          </div>
+
+          <div className="column sec-bg">
+            <h2 className="center">Now prepping</h2>
+            <div className="flex-wrap">
+              <p className="terti-bg sec-txt">104</p>
+              <p className="terti-bg sec-txt">105</p>
+              <p className="terti-bg sec-txt">106</p>
+              <p className="terti-bg sec-txt">107</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2 className="center">Our beers tonight</h2>
+          <div className="grid-2">
+            <div className="column">
+              <p>Beer no. 1</p>
+              <p>Beer no. 2</p>
+              <p>Beer no. 3</p>
+            </div>
+            <div className="column">
+              <p>Beer no. 4</p>
+              <p>Beer no. 5</p>
+              <p>Beer no. 6</p>
+            </div>
+          </div>
+          <div className="column"></div>
+        </section>
+
+        <section>
+          <div className="grid-2">
+            <div className="column">
+              <h2 className="center">Our amazing crew</h2>
+              <div className="flex-justify">
+                <p>Jonas</p>
+                <p>Peter T.</p>
+                <p>Dannie</p>
+                <p>Klaus</p>
+              </div>
+            </div>
+            <div className="column">
+              <h2 className="center">Favourites</h2>
+              <p>Beer no. 3</p>
+              <p>Beer no. 7</p>
+              <p>Beer no. 2</p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
