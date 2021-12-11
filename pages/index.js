d3ecc63855953dae3926a120dd06cf509065e504
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2021-12-11 21:08:11 +0100

    Playing around with grids and styling on front page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Bartenders from "../components/Bartenders";
 import BeersTonight from "../components/BeersTonight";
 import Countdown from "../components/Countdown";
+import Favourites from "../components/Favourites";
 import NowPrepping from "../components/NowPrepping";
 import NowServing from "../components/NowServing";
 import useDashboardDetails from "../hooks/useDashboardDetails";
@@ -21,14 +22,14 @@ export default function Dashboard() {
           <h1 className="center">Foobar</h1>
           <Countdown />
           <section className="grid-2-mobile">
-            <div className="column sec-bg">
+            <div className="column sec-bg rounded-corners">
               <h2 className="center">Now serving</h2>
               <div className="flex-wrap">
                 <NowServing serving={data.serving} />
               </div>
             </div>
 
-            <div className="column sec-bg">
+            <div className="column sec-bg rounded-corners">
               <h2 className="center">Now prepping</h2>
               <div className="flex-wrap">
                 <NowPrepping queue={data.queue} />
@@ -36,24 +37,20 @@ export default function Dashboard() {
             </div>
           </section>
 
-          <section>
+          <section className="sec-bg rounded-corners">
             <h2 className="center">Our beers tonight</h2>
             <BeersTonight />
           </section>
 
           <section>
             <div className="grid-2">
-              <div className="column">
+              <div className="column sec-bg rounded-corners">
                 <h2 className="center">Our amazing crew</h2>
                 <Bartenders bartenders={data.bartenders} />
               </div>
-              <div className="column">
+              <div className="column sec-bg rounded-corners">
                 <h2 className="center">Favourites</h2>
-                <div className="flex-justify">
-                  <h3>Sleighride</h3>
-                  <h3>Ruined childhood</h3>
-                  <h3>Steampunk</h3>
-                </div>
+                <Favourites />
               </div>
             </div>
           </section>
