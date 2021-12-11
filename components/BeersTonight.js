80654a62110dd7eb0f9243d5e01625a897c801de
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2021-12-11 21:05:03 +0100

    Only show beers currently on tap

--- components/BeersTonight.js
+++ components/BeersTonight.js
@@ -1,28 +1,23 @@
 import Image from "next/image";
-import useBeers from "../hooks/useBeers";
-
-// function generateImagePath(name) {
-//   const lowerName = name.toLowerCase().replaceAll(" ", "");
-//   const namePath = `/images/${lowerName}.jpg`;
-//   return namePath;
-// }
-
-// function imagePath(label) {
-//   const beerPath = `/images/${beer.label}.png`;
-//   return beerPath;
-// }
+import useBeersTonight from "../hooks/useBeersTonight";
 
 export default function BeersTonight() {
-  const { data, error } = useBeers();
-  if (!data) return <div>Loading...</div>;
-  if (error) return <div>An error has occured...</div>;
+  const beersTonight = useBeersTonight();
+  if (!beersTonight) return <div>Loading...</div>;
 
   return (
     <div className="grid-2">
-      {data.map((beer) => (
-        <article key={beer.name}>
-          <Image src={`/images/${beer.label}`} alt={`${beer.name} - A lovely beer`} width={150} height={150} />
-          <h3>{beer.name}</h3>
+      {beersTonight.map((beer) => (
+        <article key={beer.name} className="grid-1-3">
+          <div>
+            <Image src={`/images/${beer.label}`} alt={`${beer.name} - A lovely beer`} width={150} height={150} />
+          </div>
+          <div>
+            <h3>{beer.name}</h3>
+            <p>{beer.category}</p>
+            <p>{`${beer.alc}%`}</p>
+            <p>Kr. 49,-</p>
+          </div>
         </article>
       ))}
     </div>
